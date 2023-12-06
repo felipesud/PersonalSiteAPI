f2d226d4a15231969bf9b01a317961de1827274f
Felipe <felipesantos.dsb@hotmail.com>
2023-12-05 21:06:03 -0300

    add constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 const constants = {
     DEFAULT_PORT: 8083,
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabaseremote',
    
   };
   
